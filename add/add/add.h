2a32eebadf0d46d6e8696336c2aae4f1779313e8
better09 <33158825+better09@users.noreply.github.com>
2019-09-17 17:10:12 +0800

    Delete add.h

--- add/add/add.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADD_H
-#define ADD_H
-extern int add(int a,int b);
-extern int jianfa(int a, int b);
-extern int chengfa(int a, int b);
-extern int chufa(int a, int b);
-extern int sum(int a, int N);
-#endif
